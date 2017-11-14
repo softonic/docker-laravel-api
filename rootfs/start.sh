a29990749a3075308ff47460d3155dfda446b08d
Javi Eurrutia <javier.eurrutia@softonic.com>
2017-11-14 09:36:15 +0100

    Configured base docker container image

---
+++ rootfs/start.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -z ${EXECUTE_DEV_INSTALL+x} ]; then
+    composer install --no-interaction
+    composer clear-cache
+fi
+if [ ! -z ${EXECUTE_LARAVEL_OPTIMIZATION+x} ]; then
+    php artisan config:cache
+    php artisan route:cache
+    php artisan vendor:publish --tag=public
+fi
+
+if [ ! -z ${EXECUTE_MIGRATIONS+x} ]; then
+    php artisan migrate:refresh --seed
+fi
+
+exec "$@"
