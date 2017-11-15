b79228d0b534cf13bc6efae5482959430a106d82
Javi Eurrutia <javier.eurrutia@softonic.com>
2017-11-15 14:24:06 +0100

    Removed elif condition to else in start.sh

--- rootfs/start.sh
+++ rootfs/start.sh
@@ -14,7 +14,7 @@ fi
 
 if [ ! -z ${EXECUTE_REFRESH_MIGRATIONS+x} ]; then
     php artisan migrate:refresh --seed
-elif [ -z ${DO_NOT_EXECUTE_MIGRATIONS+x} ]; then
+else
     php artisan migrate --force
 fi
 
